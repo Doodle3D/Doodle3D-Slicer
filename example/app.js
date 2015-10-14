d367fbe02194cab750d8192efb10f0e26e965508
casperlamboo <casperlamboo@gmail.com>
2015-10-14 17:11:29 +0200

    updated to three.js r72

--- example/app.js
+++ example/app.js
@@ -3,19 +3,17 @@ import PRINTER_SETTINGS from 'settings/printer_settings.json!';
 import USER_SETTINGS from 'settings/user_settings.json!';
 import * as SLICER from 'src/index';
 
-setTimeout(() => {
-	var settings = new SLICER.Settings();
-	settings.updateConfig(PRINTER_SETTINGS["ultimaker2go"]);
-	settings.updateConfig(USER_SETTINGS);
+var settings = new SLICER.Settings();
+settings.updateConfig(PRINTER_SETTINGS["ultimaker2go"]);
+settings.updateConfig(USER_SETTINGS);
 
-	var geometry = new THREE.TorusGeometry(20, 10, 30, 30);
+var geometry = new THREE.TorusGeometry(20, 10, 30, 30);
 
-	var slicer = new SLICER.Slicer();
-	//var slicer = new SLICER.SlicerWorker();
+var slicer = new SLICER.Slicer();
+//var slicer = new SLICER.SlicerWorker();
 
-	slicer.setGeometry(geometry.clone());
-	slicer.onfinish = function (gCode) {
-		document.getElementById('gcode').innerHTML = gCode.replace(/(?:\r\n|\r|\n)/g, '<br />');
-	};
-	slicer.slice(settings);
-});
+slicer.setGeometry(geometry.clone());
+slicer.onfinish = function (gCode) {
+	document.getElementById('gcode').innerHTML = gCode.replace(/(?:\r\n|\r|\n)/g, '<br />');
+};
+slicer.slice(settings);
