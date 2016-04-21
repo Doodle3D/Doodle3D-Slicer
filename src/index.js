386a53849f189a90e0827a9db7d4615da7a8e842
casperlamboo <casperlamboo@gmail.com>
2016-04-21 22:22:59 +0200

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import Slicer from './slicer.js';
 import SlicerWorker from './slicerworker.js';
 import Settings from './settings.js';
-import ClipperLib from 'clipper-lib';
 
-ClipperLib.Error = function (message) {
-	console.error(message);
-};
-
-export {Slicer, SlicerWorker, Settings};
+export { Slicer, SlicerWorker, Settings };
