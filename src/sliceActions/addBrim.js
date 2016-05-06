e16a5dda9006f9a3f50280f87203eec428e44d05
casperlamboo <casperlamboo@gmail.com>
2016-05-06 19:52:31 +0200

    move brim code to separate function

---
+++ src/sliceActions/addBrim.js
@@ -0,0 +1,18 @@
+import THREE from 'three.js';
+import { PRECISION } from '../constants.js';
+
+const offsetOptions = {
+  jointType: 'jtSquare',
+  endType: 'etClosedPolygon',
+  miterLimit: 2.0,
+  roundPrecision: 0.25
+};
+
+export default function addBrim(slices, settings) {
+  console.log('add brim');
+
+  const brimOffset = settings.config['brimOffset'] / PRECISION;
+
+  const fistLayer = slices[0];
+  fistLayer.brim = fistLayer.getOutline().offset(brimOffset, offsetOptions);
+}
