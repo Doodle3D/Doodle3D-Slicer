b65541ff542079a67a3c2bb8dcdfe1a9ed032ec6
casperlamboo <casperlamboo@gmail.com>
2016-05-07 22:58:07 +0200

    remove slicer worker from index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Slicer from './slicer.js';
-import SlicerWorker from './slicerworker.js';
 import Settings from './settings.js';
 
-export { Slicer, SlicerWorker, Settings };
+export { Slicer, Settings };
