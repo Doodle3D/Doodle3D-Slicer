fdb5e96b6d8601dcb5f286d26bf050c64ab4136d
casperlamboo <casperlamboo@gmail.com>
2016-05-07 22:33:20 +0200

    update config

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ System.config({
     "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
