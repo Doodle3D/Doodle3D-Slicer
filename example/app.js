b7a630b57655eadfaf354feff0e79df372b5c591
casperlamboo <casperlamboo@gmail.com>
2016-05-08 11:12:35 +0200

    update example

--- example/app.js
+++ example/app.js
@@ -1,14 +1,13 @@
 import THREE from 'three.js';
 import * as SLICER from 'src/index';
 
-var settings = new SLICER.Settings();
+const settings = new SLICER.Settings();
 settings.updateConfig(SLICER.printerSettings['ultimaker2go']);
 settings.updateConfig(SLICER.userSettings);
 
-var geometry = new THREE.TorusGeometry(20, 10, 30, 30);
+const geometry = new THREE.TorusGeometry(20, 10, 30, 30);
 
-var slicer = new SLICER.Slicer();
-//var slicer = new SLICER.SlicerWorker();
+const slicer = new SLICER.Slicer();
 
 slicer.setGeometry(geometry.clone());
 slicer.addEventListener('finish', ({ gcode }) => {
