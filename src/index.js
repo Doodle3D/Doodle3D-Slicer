ef09d7216a8436365c133dc6a82c49d778bff6be
casperlamboo <casperlamboo@gmail.com>
2016-07-18 18:28:47 +0200

    update to jspm 0.17

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import Slicer from './slicer.js';
 import Settings from './settings.js';
-import printerSettings from './settings/printer_settings.json!';
-import userSettings from './settings/user_settings.json!';
+import printerSettings from './settings/printer_settings.json!json';
+import userSettings from './settings/user_settings.json!json';
 
 export { Slicer, Settings, printerSettings, userSettings };
