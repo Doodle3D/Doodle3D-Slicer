bf9d98611aba67d4b4f57ec4167c7b3c59b63712
casperlamboo <casperlamboo@gmail.com>
2016-08-19 14:46:02 +0200

    implement async slicing

---
+++ src/sliceActions/sliceWorker.js
@@ -0,0 +1,25 @@
+import Settings from '../Settings.js';
+import slice from './slice.js';
+import THREE from 'three.js';
+
+const loader = new THREE.JSONLoader();
+
+self.addEventListener('message', (event) => {
+  const { message, data } = event.data;
+  switch (message) {
+    case 'SLICE': {
+      const { geometry: JSONGeometry, config } = data;
+
+      const { geometry } = new loader.parse(JSONGeometry.data);
+      const settings = new Settings(config);
+
+      const gcode = slice(geometry, settings);
+
+      self.postMessage({
+        message: 'SLICE',
+        data: { gcode }
+      });
+      break;
+    }
+  }
+}, false);
