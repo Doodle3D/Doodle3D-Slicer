e54d077db000ec0df13851c5a00c693c28384b67
casperlamboo <casperlamboo@gmail.com>
2017-07-18 12:30:56 +0200

    update example

--- example/save.js
+++ example/save.js
@@ -12,7 +12,7 @@ const settings = {
 const jsonLoader = new THREE.JSONLoader();
 jsonLoader.load('models/airplane.json', async geometry => {
   geometry.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI / -2));
-  geometry.applyMatrix(new THREE.Matrix4().setPosition(new THREE.Vector3(50, 0.1, 50)));
+  geometry.applyMatrix(new THREE.Matrix4().setPosition(new THREE.Vector3(50, -0.1, 50)));
   geometry.computeFaceNormals();
 
   const slicer = new Slicer().setGeometry(geometry);
