db0d82c396930b46288a043a4ca521be5105b0ac
casperlamboo <casperlamboo@gmail.com>
2017-12-14 15:19:09 +0100

    remove save code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import { JSONLoader } from 'three/src/loaders/JSONLoader.js';
 import { Interface } from 'doodle3d-slicer';
 import fileURL from '!url-loader!./models/shape.json';
 import { render } from 'react-dom';
-import fileSaver from 'file-saver';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import jss from 'jss';
@@ -22,11 +21,6 @@ jss.createStyleSheet({
     html: { overflow: 'hidden' }
   }
 }).attach();
-
-const downloadGCode = ({ gcode: { gcode } }) => {
-  const file = new File([gcode], 'gcode.gcode', { type: 'text/plain' });
-  fileSaver.saveAs(file);
-};
 
 const jsonLoader = new JSONLoader();
 jsonLoader.load(fileURL, geometry => {
