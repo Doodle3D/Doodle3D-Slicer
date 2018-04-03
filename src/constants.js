06ce0b0d6b896b8e0c6dacc70c26f4f711fb382a
Casper Lamboo <casperlamboo@gmail.com>
2018-04-03 14:02:58 +0200

    bump version

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const PRECISION = 0.01;
-export const VERSION = '0.0.18';
+export const VERSION = '0.0.19';
 export const LOCAL_STORAGE_KEY = 'PRINTER_SETTINGS';
 export const MIN_AREA = 1; // holes smaller as 1mm2 get removed
 export const Z_OFFSET = 0.2;
